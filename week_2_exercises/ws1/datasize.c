6653653480a7804ca794c8949597dc4cac880408
Charlie Jheeta <146708075+charliejheeta@users.noreply.github.com>
2023-10-12 08:59:03 +0000

    exercise_3

--- week_2_exercises/ws1/datasize.c
+++ week_2_exercises/ws1/datasize.c
@@ -2,7 +2,15 @@
 #include <limits.h>
 
 int main() {
-    printf (itoa(CHAR_MIN));
+    int a_int;
+    float b_float;
+    double c_double;
+    char d_char;
+
+    printf ("The size of the integer variable is %lu bytes \n", sizeof(a_int));
+    printf ("The size of the float variable is %lu bytes \n", sizeof(b_float));
+    printf ("The size of the double variable is %lu bytes \n", sizeof(c_double));
+    printf ("The size of the character variable is %lu byte \n", sizeof(d_char));
 
     return 0;
-};
+}
