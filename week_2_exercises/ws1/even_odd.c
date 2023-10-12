02de593f87e447e98f961e1e3a71aced9783bbaa
Charlie Jheeta <146708075+charliejheeta@users.noreply.github.com>
2023-10-12 09:40:03 +0000

    exercise_6

---
+++ week_2_exercises/ws1/even_odd.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int a = 1;
+
+    if(a % 2 == 1) {
+        printf ("The integer is odd \n");
+    } else {
+        printf ("The integer is even \n");
+    };
+
+    return 0;
+}
